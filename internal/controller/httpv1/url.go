05f75975ba487be127a1acc9584877ea040a84a7
suraginv <suraginv@yandex-team.ru>
2024-10-26 22:51:05 +0300

    test: add logs for debugging

--- internal/controller/httpv1/url.go
+++ internal/controller/httpv1/url.go
@@ -63,9 +63,9 @@ func (ur *urlRoutes) saveURL(w http.ResponseWriter, r *http.Request) {
 
 	contentEncoding := r.Header.Get("Content-Encoding")
 	sendsGzip := strings.Contains(contentEncoding, "gzip")
+	ur.log.Info().Str("encoding", contentEncoding).Msg("url encoding check")
 
 	if sendsGzip {
-		ur.log.Info().Msg("start decoding")
 		gz, err := gzip.NewReader(r.Body)
 		if err != nil {
 			http.Error(w, "gzip decoding error", http.StatusInternalServerError)
