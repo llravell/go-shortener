319c2a80837c59f1c469f1b486dfedf358a93801
suraginv <suraginv@yandex-team.ru>
2024-12-04 17:37:12 +0300

    refactor: mock codegen

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -6,21 +6,22 @@ import (
 	"github.com/llravell/go-shortener/internal/entity"
 )
 
-//go:generate ../../bin/mockgen -destination=../mocks/mock_urlrepo.go -package=mocks . URLRepo
-type URLRepo interface {
-	Store(ctx context.Context, url *entity.URL) (*entity.URL, error)
-	StoreMultipleURLs(ctx context.Context, urls []*entity.URL) error
-	GetURL(ctx context.Context, hash string) (*entity.URL, error)
-	GetUserURLS(ctx context.Context, userUUID string) ([]*entity.URL, error)
-	DeleteMultipleURLs(ctx context.Context, userUUID string, urlHashes []string) error
-}
+//go:generate ../../bin/mockgen -destination=../mocks/mock_usecase.go -package=mocks . URLRepo,HealthRepo,HashGenerator
 
-//go:generate ../../bin/mockgen -destination=../mocks/mock_healthrepo.go -package=mocks . HealthRepo
-type HealthRepo interface {
-	PingContext(ctx context.Context) error
-}
+type (
+	URLRepo interface {
+		Store(ctx context.Context, url *entity.URL) (*entity.URL, error)
+		StoreMultipleURLs(ctx context.Context, urls []*entity.URL) error
+		GetURL(ctx context.Context, hash string) (*entity.URL, error)
+		GetUserURLS(ctx context.Context, userUUID string) ([]*entity.URL, error)
+		DeleteMultipleURLs(ctx context.Context, userUUID string, urlHashes []string) error
+	}
 
-//go:generate ../../bin/mockgen -destination=../mocks/mock_hashgen.go -package=mocks . HashGenerator
-type HashGenerator interface {
-	Generate() (string, error)
-}
+	HealthRepo interface {
+		PingContext(ctx context.Context) error
+	}
+
+	HashGenerator interface {
+		Generate() (string, error)
+	}
+)
