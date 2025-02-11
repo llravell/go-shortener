f92891371e3097e9a752d40709b01f955ee25dd4
Vladimir Suragin <v.suragin@yclients.tech>
2025-02-11 23:29:11 +0300

    feat: create custom multichecker

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/timakin/bodyclose/passes/bodyclose"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/appends"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	"golang.org/x/tools/go/analysis/passes/directive"
+	"golang.org/x/tools/go/analysis/passes/findcall"
+	"golang.org/x/tools/go/analysis/passes/httpmux"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilness"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"honnef.co/go/tools/staticcheck"
+)
+
+func main() {
+	mychecks := []*analysis.Analyzer{
+		printf.Analyzer,
+		shadow.Analyzer,
+		structtag.Analyzer,
+		appends.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		bools.Analyzer,
+		buildtag.Analyzer,
+		copylock.Analyzer,
+		deepequalerrors.Analyzer,
+		defers.Analyzer,
+		directive.Analyzer,
+		findcall.Analyzer,
+		httpmux.Analyzer,
+		httpresponse.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		tests.Analyzer,
+		nilness.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		bodyclose.Analyzer,
+	}
+
+	staticcheckIncludes := map[string]bool{
+		"S1001":  true,
+		"S1008":  true,
+		"ST1005": true,
+		"QF1002": true,
+		"QF1006": true,
+	}
+
+	for _, v := range staticcheck.Analyzers {
+		if strings.HasPrefix(v.Analyzer.Name, "SA") {
+			mychecks = append(mychecks, v.Analyzer)
+		} else if staticcheckIncludes[v.Analyzer.Name] {
+			mychecks = append(mychecks, v.Analyzer)
+		}
+	}
+
+	multichecker.Main(
+		mychecks...,
+	)
+}
